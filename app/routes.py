2915ec32c7136b06595b5c68a6c58f081366f3d5
Emmett D. Bondoe <edecargarbondoe@gmail.com>
2023-11-17 01:38:49 -0800

    task code

--- app/routes.py
+++ app/routes.py
@@ -6,6 +6,7 @@ from app import app, jwt, api
 from app.resources.user import UserRegister, UserLogin, UserDetailsResource,  \
     UserPasswordUpdateResource, UserDeleteResource 
 from app.resources.task import TaskResource
+from app.resources.task import TaskUpdateResource, TaskDetailsResource, TaskDeleteResource
 from app.resources.comment import CommentResource
 from app.resources.notification import NotificationResource
 
@@ -16,6 +17,9 @@ api.add_resource(UserDetailsResource, "/user-details/<int:user_id>")
 api.add_resource(UserPasswordUpdateResource, "/Update-password")
 api.add_resource(UserDeleteResource, "/user-delete/<int:user_id>")
 api.add_resource(TaskResource, "/create_task")
+api.add_resource(TaskUpdateResource, '/update_task/<task_id>', endpoint='update_task_endpoint')
+api.add_resource(TaskDetailsResource, "/task-details/<int:task_id>")
+api.add_resource(TaskDeleteResource, "/task-delete/<int:task_id>") 
 api.add_resource(CommentResource, "/create_comment")
 api.add_resource(NotificationResource, "/create_notification")
 
